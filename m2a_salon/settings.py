1fbea2c26fd6ad57b50d6ba66d1f815c09d31eff
João Mariano <marianojoao52@gmail.com>
2025-08-07 01:33:02 -0300

    feat: login page

--- m2a_salon/settings.py
+++ m2a_salon/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'm2a_salon_app',
+    'accounts',
 ]
 
 MIDDLEWARE = [
@@ -139,3 +140,6 @@ STATICFILES_DIRS = [
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+LOGIN_REDIRECT_URL = '/'
+LOGOUT_REDIRECT_URL = '/login/'
--- m2a_salon/urls.py
+++ m2a_salon/urls.py
@@ -19,6 +19,7 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('accounts/', include('accounts.urls', namespace='accounts')),
     path('', include('m2a_salon_app.urls')),
 ]
 
